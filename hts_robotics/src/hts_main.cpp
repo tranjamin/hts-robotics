4ad9405d6ffc07c9305ee653ad8269e97d7ea575
tranjamin <benjamin.tran@outlook.com.au>
2026-02-03 15:28:58 +1000

    add extra actions

--- hts_robotics/src/hts_main.cpp
+++ hts_robotics/src/hts_main.cpp
@@ -42,7 +42,9 @@ class hts_node : public rclcpp::Node {
 public:
 
   // type definitions
-  using CustomAction = hts_robotics::action::MoveToPoint;
+  using CustomActionPoint = hts_robotics::action::MoveToPoint;
+  using CustomActionPickup = hts_robotics::action::PickUpTarget;
+  using CustomActionMove = hts_robotics::action::MoveTarget;
   using StampedPoint = geometry_msgs::msg::PointStamped;
   using StampedPose = geometry_msgs::msg::PoseStamped;
   using JointState = sensor_msgs::msg::JointState;
@@ -88,22 +90,42 @@ public:
 
     // create moveit server
     RCLCPP_INFO(this->get_logger(), "Creating MoveIt2 Server...");
-    moveit_server_ = rclcpp_action::create_server<CustomAction>(
+    moveit_server_ = rclcpp_action::create_server<CustomActionPoint>(
       this, "hts_moveit_action",
-      std::bind(&hts_node::handle_goal_, this, std::placeholders::_1, std::placeholders::_2),
-      std::bind(&hts_node::handle_cancel_, this, std::placeholders::_1),
-      std::bind(&hts_node::handle_accepted_, this, std::placeholders::_1)
+      std::bind(&hts_node::handle_goal_point_, this, std::placeholders::_1, std::placeholders::_2),
+      std::bind(&hts_node::handle_cancel_point_, this, std::placeholders::_1),
+      std::bind(&hts_node::handle_accepted_point_, this, std::placeholders::_1)
     );
     RCLCPP_INFO(this->get_logger(), "Created MoveIt2 Server.");
 
     RCLCPP_INFO(this->get_logger(), "Creating MoveIt2 Client...");
-    moveit_client_ = rclcpp_action::create_client<CustomAction>(this, "hts_moveit_action");    
+    moveit_client_ = rclcpp_action::create_client<CustomActionPoint>(this, "hts_moveit_action");    
     if (!moveit_client_->wait_for_action_server(5s)) {
       RCLCPP_ERROR(this->get_logger(), "Failed to connect to action server");
       rclcpp::shutdown();
       return;
     }
     RCLCPP_INFO(this->get_logger(), "Created MoveIt2 Client.");
+
+    // create pickup server
+    RCLCPP_INFO(this->get_logger(), "Creating Pickup Server...");
+    pickup_server_ = rclcpp_action::create_server<CustomActionPickup>(
+      this, "hts_pickup_action",
+      std::bind(&hts_node::handle_goal_pickup_, this, std::placeholders::_1, std::placeholders::_2),
+      std::bind(&hts_node::handle_cancel_pickup_, this, std::placeholders::_1),
+      std::bind(&hts_node::handle_accepted_pickup_, this, std::placeholders::_1)
+    );
+    RCLCPP_INFO(this->get_logger(), "Created Pickup Server.");
+
+    // create move server
+    RCLCPP_INFO(this->get_logger(), "Creating Move Server...");
+    move_server_ = rclcpp_action::create_server<CustomActionMove>(
+      this, "hts_move_action",
+      std::bind(&hts_node::handle_goal_move_, this, std::placeholders::_1, std::placeholders::_2),
+      std::bind(&hts_node::handle_cancel_move_, this, std::placeholders::_1),
+      std::bind(&hts_node::handle_accepted_move_, this, std::placeholders::_1)
+    );
+    RCLCPP_INFO(this->get_logger(), "Created Move Server.");
 
     RCLCPP_INFO(this->get_logger(), "Finished constructing HTS Node.");
   }
@@ -142,9 +164,13 @@ public:
 
     // Apply orientation constraints
     moveit_msgs::msg::OrientationConstraint orientation_constraint;
-    orientation_constraint.header.frame_id = move_group_interface_->getPoseReferenceFrame();
+    // orientation_constraint.header.frame_id = move_group_interface_->getPoseReferenceFrame();
     // orientation_constraint.link_name = move_group_interface_->getEndEffectorLink();
-    orientation_constraint.link_name = "fr3_hand";
+    orientation_constraint.header.frame_id = "world"; // or base
+    orientation_constraint.link_name = "fr3_hand"; // or fr3_link8
+
+    RCLCPP_INFO(get_logger(), "EEF link: %s", move_group_interface_->getEndEffectorLink().c_str());
+    RCLCPP_INFO(get_logger(), "REF frame: %s", move_group_interface_->getPoseReferenceFrame().c_str());
 
     auto current_pose = move_group_interface_->getCurrentPose();
     orientation_constraint.orientation = current_pose.pose.orientation;
@@ -154,7 +180,7 @@ public:
     );
     orientation_constraint.absolute_x_axis_tolerance = 0.02;
     orientation_constraint.absolute_y_axis_tolerance = 0.02;
-    orientation_constraint.absolute_z_axis_tolerance = 7.0;
+    orientation_constraint.absolute_z_axis_tolerance = 0.02;
     orientation_constraint.weight = 1.0;
 
     moveit_msgs::msg::Constraints all_constraints;
@@ -175,25 +201,139 @@ private:
   rclcpp::Subscription<JointState>::SharedPtr joint_states_sub_;
   rclcpp::Publisher<StampedPose>::SharedPtr goal_pose_pub_;
 
-  rclcpp_action::Client<CustomAction>::SharedPtr moveit_client_;
-  rclcpp_action::Server<CustomAction>::SharedPtr moveit_server_;
+  rclcpp_action::Client<CustomActionPoint>::SharedPtr moveit_client_;
+  rclcpp_action::Server<CustomActionPoint>::SharedPtr moveit_server_;
 
-  rclcpp_action::GoalResponse handle_goal_(
-    const rclcpp_action::GoalUUID&, std::shared_ptr<const CustomAction::Goal> goal
+  rclcpp_action::Server<CustomActionPickup>::SharedPtr pickup_server_;
+  rclcpp_action::Server<CustomActionMove>::SharedPtr move_server_;
+
+  // Server Callbacks for Pickup Target Action
+  rclcpp_action::GoalResponse handle_goal_pickup_(
+    const rclcpp_action::GoalUUID&, std::shared_ptr<const CustomActionPickup::Goal> goal
   ) {
-    RCLCPP_INFO(this->get_logger(), "Received goal request (%.2f %.2f %.2f)", goal->x, goal->y, goal->z);
+    RCLCPP_INFO(this->get_logger(), "Received pickup request Position (%.2f %.2f %.2f) and Orientation (%.2f %.2f %.2f %.2f)", goal->x, goal->y, goal->z, goal->ox, goal->oy, goal->oz, goal->ow);
     return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
   }
 
-  rclcpp_action::CancelResponse handle_cancel_(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomAction>> goal_handle
+  rclcpp_action::CancelResponse handle_cancel_pickup_(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomActionPickup>> goal_handle
   ) {
-    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel pickup");
     return rclcpp_action::CancelResponse::REJECT;
   }
 
-  void handle_accepted_(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomAction>> goal_handle
+  void handle_accepted_pickup_(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomActionPickup>> goal_handle
+  ) {
+    std::thread([this, goal_handle]() {
+      auto goal = goal_handle->get_goal();
+
+      geometry_msgs::msg::Pose target;
+      target.position.x = goal->x;
+      target.position.y = goal->y;
+      target.position.z = goal->z;
+      target.orientation.x = goal->ox;
+      target.orientation.y = goal->oy;
+      target.orientation.z = goal->oz;
+      target.orientation.w = goal->ow;
+
+      move_group_interface_->clearPathConstraints();
+      move_group_interface_->setPoseTarget(target);
+      bool success = (move_group_interface_->move() == moveit::core::MoveItErrorCode::SUCCESS);
+
+      auto result = std::make_shared<CustomActionPickup::Result>();
+      result->success = success;
+      RCLCPP_INFO(this->get_logger(), "Goal reached successfully");
+      goal_handle->succeed(result);
+    }).detach();
+  
+  }
+
+  // Server Callbacks for Move Target Action
+  rclcpp_action::GoalResponse handle_goal_move_(
+    const rclcpp_action::GoalUUID&, std::shared_ptr<const CustomActionMove::Goal> goal
+  ) {
+    RCLCPP_INFO(this->get_logger(), "Received move request (%.2f %.2f %.2f)", goal->x, goal->y, goal->z);
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel_move_(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomActionMove>> goal_handle
+  ) {
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel move");
+    return rclcpp_action::CancelResponse::REJECT;
+  }
+
+  void handle_accepted_move_(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomActionMove>> goal_handle
+  ) {
+    std::thread([this, goal_handle]() {
+      auto goal = goal_handle->get_goal();
+
+      geometry_msgs::msg::Pose target;
+      target.position.x = goal->x;
+      target.position.y = goal->y;
+      target.position.z = goal->z;
+
+            // Apply orientation constraints
+            moveit_msgs::msg::OrientationConstraint orientation_constraint;
+            // orientation_constraint.header.frame_id = move_group_interface_->getPoseReferenceFrame();
+            // orientation_constraint.link_name = move_group_interface_->getEndEffectorLink();
+            orientation_constraint.header.frame_id = "world"; // or base
+            orientation_constraint.link_name = "fr3_hand"; // or fr3_link8
+
+            RCLCPP_INFO(get_logger(), "EEF link: %s", move_group_interface_->getEndEffectorLink().c_str());
+            RCLCPP_INFO(get_logger(), "REF frame: %s", move_group_interface_->getPoseReferenceFrame().c_str());
+
+            auto current_pose = move_group_interface_->getCurrentPose();
+            orientation_constraint.orientation = current_pose.pose.orientation;
+            RCLCPP_INFO(get_logger(), "Current Pose Orientation: (%f, %f, %f, %f)",
+              current_pose.pose.orientation.x, current_pose.pose.orientation.y,
+              current_pose.pose.orientation.z, current_pose.pose.orientation.w
+            );
+            orientation_constraint.absolute_x_axis_tolerance = 0.02;
+            orientation_constraint.absolute_y_axis_tolerance = 0.02;
+            orientation_constraint.absolute_z_axis_tolerance = 0.02;
+            orientation_constraint.weight = 1.0;
+
+            moveit_msgs::msg::Constraints all_constraints;
+            all_constraints.orientation_constraints.emplace_back(orientation_constraint);
+
+            move_group_interface_->setPathConstraints(all_constraints);
+            RCLCPP_INFO(get_logger(), "Applied orientation constraints to planning scene.");
+
+      target.orientation.x = current_pose.pose.orientation.x;
+      target.orientation.y = current_pose.pose.orientation.y;
+      target.orientation.z = current_pose.pose.orientation.z; // maybe remove this
+      target.orientation.w = current_pose.pose.orientation.w;
+
+      move_group_interface_->setPoseTarget(target);
+      bool success = (move_group_interface_->move() == moveit::core::MoveItErrorCode::SUCCESS);
+
+      auto result = std::make_shared<CustomActionMove::Result>();
+      result->success = success;
+      RCLCPP_INFO(this->get_logger(), "Goal reached successfully");
+      goal_handle->succeed(result);
+    }).detach();
+  }
+
+  // Server Callbacks for Move To Point Action
+  rclcpp_action::GoalResponse handle_goal_point_(
+    const rclcpp_action::GoalUUID&, std::shared_ptr<const CustomActionPoint::Goal> goal
+  ) {
+    RCLCPP_INFO(this->get_logger(), "Received point request (%.2f %.2f %.2f)", goal->x, goal->y, goal->z);
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel_point_(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomActionPoint>> goal_handle
+  ) {
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel point");
+    return rclcpp_action::CancelResponse::REJECT;
+  }
+
+  void handle_accepted_point_(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<CustomActionPoint>> goal_handle
   ) {
     std::thread([this, goal_handle]() {
       auto goal = goal_handle->get_goal();
@@ -206,25 +346,24 @@ private:
       move_group_interface_->setPoseTarget(target);
       bool success = (move_group_interface_->move() == moveit::core::MoveItErrorCode::SUCCESS);
 
-      auto result = std::make_shared<hts_robotics::action::MoveToPoint::Result>();
+      auto result = std::make_shared<CustomActionPoint::Result>();
       result->success = success;
       RCLCPP_INFO(this->get_logger(), "Goal reached successfully");
       goal_handle->succeed(result);
     }).detach();
-  
-}
+  }
 
   void clicked_point_subscriber_callback_(StampedPoint::UniquePtr msg) {
     RCLCPP_INFO(this->get_logger(), "Received Clicked Point: (%f, %f, %f)", msg->point.x, msg->point.y, msg->point.z);
 
     // create a goal message
-    auto goal_msg = CustomAction::Goal();
+    auto goal_msg = CustomActionPoint::Goal();
     goal_msg.x = 0.5; goal_msg.y = 0.0; goal_msg.z = 0.3;
 
     // sets callbacks for action
-    auto send_goal_options = rclcpp_action::Client<CustomAction>::SendGoalOptions();
+    auto send_goal_options = rclcpp_action::Client<CustomActionPoint>::SendGoalOptions();
     send_goal_options.result_callback =
-      [this](const rclcpp_action::ClientGoalHandle<CustomAction>::WrappedResult & result) {
+      [this](const rclcpp_action::ClientGoalHandle<CustomActionPoint>::WrappedResult & result) {
         if (result.code == rclcpp_action::ResultCode::SUCCEEDED) {
           RCLCPP_INFO(this->get_logger(), "Goal succeeded!");
         } else {
