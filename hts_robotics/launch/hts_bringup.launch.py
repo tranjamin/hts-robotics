ef3620e4b8eab4be53ca8df92d25d0e0d4c331ed
tranjamin <benjamin.tran@outlook.com.au>
2026-02-16 21:26:08 +1000

    spin up realsense camera for sim

--- hts_robotics/launch/hts_bringup.launch.py
+++ hts_robotics/launch/hts_bringup.launch.py
@@ -395,7 +395,11 @@ def generate_launch_description():
             "/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock",
             "/ros_gz/model/pose@ros_gz_interfaces/msg/Entity@gz.msgs.Entity",
             "/world/empty/pose/info@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
-            "/world/empty/dynamic_pose/info@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V"
+            "/world/empty/dynamic_pose/info@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
+            "/custom_topic/color/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
+            "/custom_topic/color/depth_image@sensor_msgs/msg/Image@gz.msgs.Image",
+            "/custom_topic/color/image@sensor_msgs/msg/Image@gz.msgs.Image",
+            "/custom_topic/color/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
         ],
         output="screen",
     )
@@ -422,8 +426,8 @@ def generate_launch_description():
     return LaunchDescription(all_launch_arguments + [
         gazebo_empty_world,
         # gripper_launch,
-        realsense_node,
-        anygrasp_node,
+        # realsense_node,
+        # anygrasp_node,
 
         TimerAction(
             period=5.0,
