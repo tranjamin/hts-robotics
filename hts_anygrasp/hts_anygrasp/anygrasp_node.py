3bd4231c3a55814cc23f3c362ac24368d376e31c
tranjamin <benjamin.tran@outlook.com.au>
2026-02-21 23:21:54 +1000

    cleaned up file structure, tweaked dockerfile to include octomap server and remove realsense nodes, encapsulated anygrasp params into config file

--- hts_anygrasp/hts_anygrasp/anygrasp_node.py
+++ hts_anygrasp/hts_anygrasp/anygrasp_node.py
@@ -4,7 +4,6 @@ import argparse
 import numpy as np
 import open3d as o3d
 
-from cv_bridge import CvBridge
 from ament_index_python.packages import get_package_prefix
 from scipy.spatial.transform import Rotation
 
@@ -12,7 +11,7 @@ import rclpy
 from rclpy.qos import QoSProfile, ReliabilityPolicy
 from rclpy.node import Node
 from hts_msgs.srv import RequestGrasp, DisplayCloud
-from sensor_msgs.msg import PointCloud2, Image
+from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import Pose
 
 pkg_prefix = get_package_prefix("hts_anygrasp")
@@ -23,49 +22,88 @@ checkpoint_path = os.path.join(pkg_prefix, "share/hts_anygrasp/checkpoint_detect
 from graspnetAPI import GraspGroup
 from gsnet import AnyGrasp
 
-qos = QoSProfile(depth=10)
-qos.reliability = ReliabilityPolicy.BEST_EFFORT
-
-from_file = np.load("/ros2_ws/src/full_cloud_1771641840.npz")
-from_file_points = from_file['points']
-
 class AnyGraspNode(Node):
-    Z_COORDS_MIN = 0.001
-    Z_COORDS_MAX = 0.5
-    X_GRASP_MIN = -1.0
-    X_GRASP_MAX = 1.0
-    Y_GRASP_MIN = -1.0
-    Y_GRASP_MAX = 1.0
-    Z_GRASP_MIN = 0.03
-    Z_GRASP_MAX = 0.5
-
-    APPLY_OBJECT_MASK = True
-    APPLY_COLLISIONS = True
-    DENSE_GRASP = True
-
-    MIN_GRASP_WIDTH = 0.01
-    MAX_GRASP_PITCH_ROLL_DEG = 5
-
-    NMS_TRANSLATION_THRESH = 0.005
-    NMS_ANGLE_THRESH_DEG = 5
-
-    MASK_RADIUS = 0.05
-
-    def __init__(self, args):
+    def __init__(self):
         super().__init__('hts_anygrasp')
+
+        # DECLARE PARAMETERS
+        self.declare_parameter('z_coords_min', 0.001)
+        self.declare_parameter('z_coords_max', 100.0)
+        self.declare_parameter('x_grasp_min', -1.0)
+        self.declare_parameter('x_grasp_max', 1.0)
+        self.declare_parameter('y_grasp_min', -1.0)
+        self.declare_parameter('y_grasp_max', 1.0)
+        self.declare_parameter('z_grasp_min', 0.03)
+        self.declare_parameter('z_grasp_max', 1.0)
+        self.declare_parameter('apply_object_mask', True)
+        self.declare_parameter('apply_collisions', True)
+        self.declare_parameter('dense_grasp', True)
+        self.declare_parameter('min_grasp_width', 0.01)
+        self.declare_parameter('max_grasp_pitch_roll_deg', 5.0)
+        self.declare_parameter('nms_translation_thresh', 0.005)
+        self.declare_parameter('nms_angle_thresh_deg', 5.0)
+        self.declare_parameter('pointcloud_from_file', False)
+        self.declare_parameter('pointcloud_file', '')
+        self.declare_parameter('mask_radius', 0.05)
+        self.declare_parameter('no_rgb', True)
+        self.declare_parameter('pointcloud_topic', '/octomap_point_cloud_centres')
+        self.declare_parameter('visualise', True)
+        self.declare_parameter('max_gripper_width', 0.1)
+        self.declare_parameter('gripper_height', 0.03)
+        self.declare_parameter('top_down_grasp', False)
+        self.declare_parameter('grasp_z_offset', 0.02)
+        self.declare_parameter('grasp_axis_offset', 0.14)
+
+        self.Z_COORDS_MIN = self.get_parameter('z_coords_min').value
+        self.Z_COORDS_MAX = self.get_parameter('z_coords_max').value
+        self.X_GRASP_MIN = self.get_parameter('x_grasp_min').value 
+        self.X_GRASP_MAX = self.get_parameter('x_grasp_max').value
+        self.Y_GRASP_MIN = self.get_parameter('y_grasp_min').value 
+        self.Y_GRASP_MAX = self.get_parameter('y_grasp_max').value
+        self.Z_GRASP_MIN = self.get_parameter('z_grasp_min').value 
+        self.Z_GRASP_MAX = self.get_parameter('z_grasp_max').value
+        self.APPLY_OBJECT_MASK = self.get_parameter('apply_object_mask').value
+        self.APPLY_COLLISIONS = self.get_parameter('apply_collisions').value
+        self.DENSE_GRASP = self.get_parameter('dense_grasp').value
+        self.MIN_GRASP_WIDTH = self.get_parameter('min_grasp_width').value
+        self.MAX_GRASP_PITCH_ROLL_DEG = self.get_parameter('max_grasp_pitch_roll_deg').value
+        self.NMS_TRANSLATION_THRESH = self.get_parameter('nms_translation_thresh').value
+        self.NMS_ANGLE_THRESH_DEG = self.get_parameter('nms_angle_thresh_deg').value
+        self.POINTCLOUD_FROM_FILE = self.get_parameter('pointcloud_from_file').value
+        self.POINTCLOUD_FILE = self.get_parameter('pointcloud_file').value
+        self.MASK_RADIUS = self.get_parameter('mask_radius').value
+        self.NO_RGB = self.get_parameter('no_rgb').value
+        self.POINTCLOUD_TOPIC = self.get_parameter('pointcloud_topic').value
+        self.VISUALISE = self.get_parameter('visualise').value
+        self.MAX_GRIPPER_WIDTH = self.get_parameter('max_gripper_width').value
+        self.GRIPPER_HEIGHT = self.get_parameter('gripper_height').value
+        self.TOP_DOWN_GRASP = self.get_parameter('top_down_grasp').value
+        self.GRASP_Z_OFFSET = self.get_parameter('grasp_z_offset').value
+        self.GRASP_AXIS_OFFSET = self.get_parameter('grasp_axis_offset').value
+
         self.depth_pointcloud_: PointCloud2 = None
-        self.depth_image_: Image = None
-        self.rgb_image_: Image = None
-        
-        self.anygrasp = AnyGrasp(args)
+
+        if self.POINTCLOUD_FROM_FILE:
+            from_file = np.load(self.POINTCLOUD_FILE)
+            self.file_points = from_file['points']
+            if not self.NO_RGB:
+                self.file_colors = from_file['colors']
+
+        cfgs = argparse.Namespace(
+            checkpoint_path=checkpoint_path,
+            max_gripper_width=self.MAX_GRIPPER_WIDTH,
+            gripper_height=self.GRIPPER_HEIGHT,
+            top_down_grasp=self.TOP_DOWN_GRASP,
+            debug=True,
+        )
+        cfgs.max_gripper_width = max(0, min(0.1, cfgs.max_gripper_width))
+        self.anygrasp = AnyGrasp(cfgs)
         self.anygrasp.load_net()
 
-        self.cfgs = args
-        self.bridge = CvBridge()
+        qos = QoSProfile(depth=10)
+        qos.reliability = ReliabilityPolicy.BEST_EFFORT
 
-        self.NORGB = True
-        # self.pointcloud_listener_ = self.create_subscription(PointCloud2, "/camera_sim/points", self.pointcloud_callback_, 1)
-        self.pointcloud_listener_ = self.create_subscription(PointCloud2, "/octomap_point_cloud_centers", self.pointcloud_callback_, qos)
+        self.pointcloud_listener_ = self.create_subscription(PointCloud2, self.POINTCLOUD_TOPIC, self.pointcloud_callback_, qos)
         self.grasp_service_ = self.create_service(RequestGrasp, 'request_grasp', self.grasp_callback_)
         self.display_service_ = self.create_service(DisplayCloud, 'display_cloud', self.display_callback_)
 
@@ -107,7 +145,7 @@ class AnyGraspNode(Node):
         if self.depth_pointcloud_ is None:
             self.get_logger().info("Found no points")
 
-        if self.NORGB:
+        if self.NO_RGB:
             points, colors = self.fast_norgb_pc2_to_numpy(self.depth_pointcloud_)
         else:
             points, colors = self.fast_pc2_to_numpy(self.depth_pointcloud_)
@@ -122,12 +160,11 @@ class AnyGraspNode(Node):
         return response
 
     def fast_norgb_pc2_to_numpy(self, msg):
-        # structured dtype matching your fields
         dtype = np.dtype([
             ('x', np.float32),
             ('y', np.float32),
             ('z', np.float32),
-            ('pad', np.float32),   # offset 12–15 unused
+            ('pad', np.float32),
         ])
 
         cloud = np.frombuffer(msg.data, dtype=dtype)
@@ -143,7 +180,7 @@ class AnyGraspNode(Node):
             ('x', np.float32),
             ('y', np.float32),
             ('z', np.float32),
-            ('pad', np.float32),   # offset 12–15 unused
+            ('pad', np.float32),
             ('rgb', np.float32),
             ('pad2', np.float32),
         ])
@@ -166,21 +203,28 @@ class AnyGraspNode(Node):
         return points.astype(np.float32), colors.astype(np.float32)
 
     def generate_pose_(self, x, y, z, radius):
-        if self.NORGB:
-            points, colors = self.fast_norgb_pc2_to_numpy(self.depth_pointcloud_)
-            AnyGraspNode.display_pointcloud(points, save=True, filename="full_cloud")
-            points = from_file_points
-            colors = np.zeros_like(points, dtype=np.float32)
+        if self.NO_RGB:
+            if not self.POINTCLOUD_FROM_FILE:
+                points, colors = self.fast_norgb_pc2_to_numpy(self.depth_pointcloud_)
+            else:
+                points = self.file_points
+                colors = np.zeros_like(points, dtype=np.float32)
+            if self.VISUALISE:
+                AnyGraspNode.display_pointcloud(points, save=True, filename="full_cloud")
         else:
-            points, colors = self.fast_pc2_to_numpy(self.depth_pointcloud_)
-            AnyGraspNode.display_pointcloud(points, colors, save=True, filename="full_cloud")
-
+            if not self.POINTCLOUD_FROM_FILE:
+                points, colors = self.fast_pc2_to_numpy(self.depth_pointcloud_)
+            else:
+                points = self.file_points
+                colors = self.file_colors
+            if self.VISUALISE:
+                AnyGraspNode.display_pointcloud(points, colors, save=True, filename="full_cloud")
 
         # filter according to z
         z_coords = points[:, 2]
         y_coords = points[:, 1]
         x_coords = points[:, 0]
-        mask = (z_coords > AnyGraspNode.Z_COORDS_MIN) & (z_coords < AnyGraspNode.Z_COORDS_MAX) & ((x - x_coords)**2 + (y - y_coords)**2 < radius**2)
+        mask = (z_coords > self.Z_COORDS_MIN) & (z_coords < self.Z_COORDS_MAX) & ((x - x_coords)**2 + (y - y_coords)**2 < radius**2)
         
         cropped_points = points[mask].astype(np.float32)
         cropped_colors = colors[mask].astype(np.float32)
@@ -192,25 +236,26 @@ class AnyGraspNode(Node):
             return None
 
         # show cropped and uncropped pointclouds
-        if self.NORGB:
-            AnyGraspNode.display_pointcloud(cropped_points)
-            AnyGraspNode.display_pointcloud(uncropped_points)
-        else:
-            AnyGraspNode.display_pointcloud(cropped_points, cropped_colors)
-            AnyGraspNode.display_pointcloud(uncropped_points, uncropped_colors)
+        if self.VISUALISE:
+            if self.NO_RGB:
+                AnyGraspNode.display_pointcloud(cropped_points)
+                AnyGraspNode.display_pointcloud(uncropped_points)
+            else:
+                AnyGraspNode.display_pointcloud(cropped_points, cropped_colors)
+                AnyGraspNode.display_pointcloud(uncropped_points, uncropped_colors)
 
         # set workspace to filter output grasps
-        xmin, xmax = AnyGraspNode.X_GRASP_MIN, AnyGraspNode.X_GRASP_MAX
-        ymin, ymax = AnyGraspNode.Y_GRASP_MIN, AnyGraspNode.Y_GRASP_MAX
-        zmin, zmax = AnyGraspNode.Z_GRASP_MIN, AnyGraspNode.Z_GRASP_MAX
+        xmin, xmax = self.X_GRASP_MIN, self.X_GRASP_MAX
+        ymin, ymax = self.Y_GRASP_MIN, self.Y_GRASP_MAX
+        zmin, zmax = self.Z_GRASP_MIN, self.Z_GRASP_MAX
         lims = [xmin, xmax, ymin, ymax, zmin, zmax]
 
         gg, cloud = self.anygrasp.get_grasp(
             cropped_points, cropped_colors, 
             lims=lims, 
-            apply_object_mask=AnyGraspNode.APPLY_OBJECT_MASK, 
-            dense_grasp=AnyGraspNode.DENSE_GRASP, 
-            collision_detection=AnyGraspNode.APPLY_COLLISIONS
+            apply_object_mask=self.APPLY_OBJECT_MASK, 
+            dense_grasp=self.DENSE_GRASP, 
+            collision_detection=self.APPLY_COLLISIONS
             )
 
         if gg is None or len(gg) == 0:
@@ -220,16 +265,16 @@ class AnyGraspNode(Node):
         exclude_grasps = []
         for ind, grasp in enumerate(gg):
             # exclude grasps by width
-            if grasp.width < AnyGraspNode.MIN_GRASP_WIDTH:
+            if grasp.width < self.MIN_GRASP_WIDTH:
                 exclude_grasps.append(ind)
                 continue
 
             # exclude grasps by orientation
             roll, pitch, yaw = Rotation.from_matrix(grasp.rotation_matrix).as_euler('xyz', degrees=True)
-            if abs(pitch) > AnyGraspNode.MAX_GRASP_PITCH_ROLL_DEG and abs(pitch - 180) > AnyGraspNode.MAX_GRASP_PITCH_ROLL_DEG:
+            if abs(pitch) > self.MAX_GRASP_PITCH_ROLL_DEG and abs(pitch - 180) > self.MAX_GRASP_PITCH_ROLL_DEG:
                 exclude_grasps.append(ind)
                 continue
-            if abs(roll) > AnyGraspNode.MAX_GRASP_PITCH_ROLL_DEG and abs(roll - 180) > AnyGraspNode.MAX_GRASP_PITCH_ROLL_DEG:
+            if abs(roll) > self.MAX_GRASP_PITCH_ROLL_DEG and abs(roll - 180) > self.MAX_GRASP_PITCH_ROLL_DEG:
                 exclude_grasps.append(ind)
                 continue
 
@@ -244,12 +289,12 @@ class AnyGraspNode(Node):
 
         # perform non-maximum suppression
         gg = gg.nms(
-            translation_thresh = AnyGraspNode.NMS_TRANSLATION_THRESH,
-            rotation_thresh = AnyGraspNode.NMS_ANGLE_THRESH_DEG / 180 * np.pi
+            translation_thresh = self.NMS_TRANSLATION_THRESH,
+            rotation_thresh = self.NMS_ANGLE_THRESH_DEG / 180 * np.pi
         ).sort_by_score()
 
         # visualization
-        if self.cfgs.debug:
+        if self.VISUALISE:
             AnyGraspNode.display_grasps(gg, cloud, origin_position=[x,y,z])
             AnyGraspNode.display_grasps(gg, cloud, only_first=True, origin_position=[x,y,z])
 
@@ -258,7 +303,7 @@ class AnyGraspNode(Node):
         return gg[0]
 
     def grasp_callback_(self, request, response):
-        if self.depth_pointcloud_ is None:
+        if not self.POINTCLOUD_FROM_FILE and self.depth_pointcloud_ is None:
             self.get_logger().info("PointCloud Not Available")
             response.success = False
             return response
@@ -288,8 +333,8 @@ class AnyGraspNode(Node):
             # y is perpendicular to z in the horizontal plane
             # x points vertical
 
-        offset_translation = np.array([0, 0, -0.14])
-        grasp.translation[2] += 0.02
+        offset_translation = np.array([0, 0, -self.GRASP_AXIS_OFFSET])
+        grasp.translation[2] += self.GRASP_Z_OFFSET
         final_translation = grasp.translation + final_rotation.as_matrix() @ offset_translation
         print(final_translation)
 
@@ -309,16 +354,7 @@ class AnyGraspNode(Node):
 def main():
     rclpy.init(args=None)
 
-    cfgs = argparse.Namespace(
-        checkpoint_path=checkpoint_path,
-        max_gripper_width=0.1,
-        gripper_height=0.03,
-        top_down_grasp=False,
-        debug=True,
-    )
-    cfgs.max_gripper_width = max(0, min(0.1, cfgs.max_gripper_width))
-
-    node = AnyGraspNode(cfgs)
+    node = AnyGraspNode()
 
     rclpy.spin(node)
     rclpy.shutdown()
